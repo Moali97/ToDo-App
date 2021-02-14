6c40e16648cb4403c691ecf4fc8a3eebbc7bc942
Syed Ali <syedali3267@gmail.com>
2021-02-14 20:03:53 +0000

    Changed DEBUG to False

--- list_project/settings.py
+++ list_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wymnn29@ogvr1#m&(ly$l47l!v0d9l87=t3o*=740)t@g!bi7%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
