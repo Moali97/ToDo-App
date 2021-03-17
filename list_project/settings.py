bdd965cfda7c5a7fd384265981961e46f55472fb
Syed Ali <syedali3267@gmail.com>
2021-03-17 19:57:48 +0000

    Secret key

--- list_project/settings.py
+++ list_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wymnn29@ogvr1#m&(ly$l47l!v0d9l87=t3o*=740)t@g!bi7%'
+SECRET_KEY = os.environ['TODOSECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
