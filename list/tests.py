af3116db410f11d2a3ce26e25687896df5ab3039
Syed Ali <syedali3267@gmail.com>
2021-03-26 20:29:54 +0000

    Adding Tests

--- list/tests.py
+++ list/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from list.models import List
+
 
 # Create your tests here.
+
+class ListTestCase(TestCase):
+
+    def setUp(self):
+        List.objects.create(text='just a test')
+
+    def test_text_content(self):
+        list = List.objects.get(id=1)
+        expected_object_name = f'{list.text}'
+        self.assertEqual(expected_object_name, 'just a test')
+
