f06596a2b020462752c23f9526fa396a81a6b587
Syed Ali <syedali3267@gmail.com>
2021-03-30 12:26:08 +0100

    Added Date Created

--- list/models.py
+++ list/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.urls import reverse
 
 
 class List(models.Model):
     title = models.CharField(max_length=250, default='')
+    date_created = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title[:250]
