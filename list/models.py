d0e0862ac61bac65cbf9fb2e82bedd93951718c5
Syed Ali <syedali3267@gmail.com>
2021-03-30 16:08:15 +0100

    Added 'Date Created'

--- list/models.py
+++ list/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class List(models.Model):
     title = models.CharField(max_length=250, default='')
-    date_created = models.DateField(auto_now_add=True)
+    date_created = models.DateField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return self.title[:250]
