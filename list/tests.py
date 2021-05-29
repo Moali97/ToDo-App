bf19d59697827dc8a9406bad0cfcded662b3b070
Syed Ali <syedali3267@gmail.com>
2021-05-29 15:03:30 +0100

    Added Tests model 'date_created'

--- list/tests.py
+++ list/tests.py
@@ -1,17 +1,21 @@
 from django.test import TestCase
 from .models import List
+from django.test import SimpleTestCase
 
 
+# tests for medels
 class TestNewLists(TestCase):
 
     def setUp(self):
         List.objects.create(title='Go shopping for food')
+        List.objects.create(date_created='2021-05-29')
 
     def test_text_content(self):
         list = List.objects.get(id=1)
         expected_object_name = f'{list.title}'
         self.assertEqual(expected_object_name, 'Go shopping for food')
 
-# self.post = List.objects.create(
-# title='Make some spicy food',
-# )
+    def test_date_content(self):
+        list = List.objects.get(id=1)
+        expected_object_name = f'{list.date_created}'
+        self.assertEqual(expected_object_name, '2021-05-29')
