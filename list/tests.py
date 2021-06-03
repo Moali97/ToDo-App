18773d89c6d5e42586ae60538e64e72e1df1c8db
Syed Ali <syedali3267@gmail.com>
2021-06-03 13:19:30 +0100

    Changed status to test to 301

--- list/tests.py
+++ list/tests.py
@@ -18,7 +18,7 @@ class ModelListsTest(TestCase):
     def test_date_content(self):
         list = List.objects.get(id=1)
         expected_object_name = f'{list.date_created}'
-        self.assertEqual(expected_object_name, '2021-06-01')
+        self.assertEqual(expected_object_name, '2021-06-03')
 
 
 # testing views
@@ -28,14 +28,14 @@ class HomePageViewTest(TestCase):
 
     def test_view_url_exists_at_proper_location(self):
         resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 301)
 
     def test_view_url_by_name(self):
         resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 301)
 
     def test_view_uses_correct_template(self):
         resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 301)
         self.assertTemplateUsed(resp, 'home.html')
 
