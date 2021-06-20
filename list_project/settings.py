d01aefb0d5b47c74b6242d390e2961c3403ad322
Syed Ali <syedali3267@gmail.com>
2021-06-20 17:12:26 +0100

    postgres configurations

--- list_project/settings.py
+++ list_project/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,6 +78,8 @@ DATABASES = {
    }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
